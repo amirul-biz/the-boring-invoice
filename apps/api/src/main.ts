6cdb9280c11427743bee22b42c22bb7285ff23fc
Amirul Irfan <amirul.irfan.biz@gmail.com>
2025-12-20 18:28:11 +0800

    main
    - fix cors issue

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://the-boring-invoice-client.vercel.app',
+      'http://localhost:4200',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('The Boring Invoice API')
